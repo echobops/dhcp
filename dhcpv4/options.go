2aa077cd945ce54ede105eaa0d11830f3dd0eb33
echobops <ops@echobroadband.net>
2020-02-08 18:56:24 -0700

    removed stale variable

--- dhcpv4/options.go
+++ dhcpv4/options.go
@@ -156,7 +156,7 @@ func (o Options) fromBytesCheckEnd(data []byte, checkEndOption bool) error {
 	}
 
 	// Any bytes left must be padding.
-	var pad uint8
+	// var pad uint8
 	for buf.Len() >= 1 {
 		_ = buf.Read8()
 		// pad = buf.Read8()
